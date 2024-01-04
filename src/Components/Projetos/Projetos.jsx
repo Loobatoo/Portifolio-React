ea4c6a6e9b12f8aee7f997bba8702af58891b6e8
Pedro Lobato <pedrolfirbida@gmail.com>
2024-01-04 15:49:36 -0300

    Projetos (Pronto)

--- src/Components/Projetos/Projetos.jsx
+++ src/Components/Projetos/Projetos.jsx
@@ -9,13 +9,21 @@ const Projetos = () => {
     <div className={classes.Container}>
         <h1> Meus Trabalhos: </h1>
         <h2> React </h2>
-        <div className={classes.ReactGrid}>
-          <CardProjetos Image="./src/Img/Banners/12.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
-          <CardProjetos Image="./src/Img/Banners/11.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
-          <CardProjetos Image="./src/Img/Banners/10.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
-          <CardProjetos Image="./src/Img/Banners/9.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
-          <CardProjetos Image="./src/Img/Banners/8.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
-        </div>
+          <div className={classes.Grid}>
+            <CardProjetos Image="./src/Img/Banners/12.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/11.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/10.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/9.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/8.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+          </div>
+        <h2> JavaScript(ES6) </h2>
+          <div className={classes.Grid}>
+            <CardProjetos Image="./src/Img/Banners/12.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/11.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/10.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/9.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+            <CardProjetos Image="./src/Img/Banners/8.png" Title="Pedro" Tech="React" Link="https://www.youtube.com"/>
+          </div>
     </div>
   )
 }
